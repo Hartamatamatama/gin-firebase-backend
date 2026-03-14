79a54fb61caaa319a2de0eaaa40d0b726b09fda7
hartamatamatama <katonojutsu340@gmail.com>
2026-03-14 12:06:27 +0700

    Memasukkan kode VerifyFirebaseToken

--- services/auth_service.go
+++ services/auth_service.go
@@ -11,3 +11,14 @@ type AuthService struct {
 func NewAuthService() *AuthService {
 	return &AuthService{userRepo: repositories.NewUserRepository()}
 }
+
+// VerifyFirebaseToken verifikasi token dari Firebase,
+// pastikan email sudah verified, lalu return Backend JWT
+func (s *AuthService) VerifyFirebaseToken(firebaseToken string) (string, *models.User, error) {
+	// 1. Verifikasi Firebase ID Token ke server Google
+	token, err := config.FirebaseAuth.VerifyIDToken(context.Background(), firebaseToken)
+	if err != nil {
+		return "", nil, errors.New("firebase token tidak valid atau kadaluarsa")
+	}
+
+	
